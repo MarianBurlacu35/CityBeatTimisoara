8f3a53b56c1e533d9cea0e1f6df367f035586e41
Marian-Cosmin <marian-cosmin.burlacu@student.tuiasi.ro>
2025-11-14 16:47:49 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,3 @@
+// main.ts
+console.log("Landing Page Loaded");
+// Poți adăuga aici logica pentru butoane, animatii, fetch pentru evenimente etc.
---
+++ main.ts
@@ -0,0 +1,5 @@
+// main.ts
+
+console.log("Landing Page Loaded");
+
+// Poți adăuga aici logica pentru butoane, animatii, fetch pentru evenimente etc.
