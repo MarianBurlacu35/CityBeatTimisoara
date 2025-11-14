c1c6918e848a0e37bdf3ca9c39c219aae8517273
Marian-Cosmin <marian-cosmin.burlacu@student.tuiasi.ro>
2025-11-14 17:19:58 +0200

    Move header files to top-level header/ and update landingPage references

--- dist/landingPage/main.js
+++ dist/landingPage/main.js
@@ -1,5 +1,4 @@
+"use strict";
 // main.ts
-
 console.log("Landing Page Loaded");
-
 // Poți adăuga aici logica pentru butoane, animatii, fetch pentru evenimente etc.
--- main.js
+++
@@ -1,3 +0,0 @@
-// main.ts
-console.log("Landing Page Loaded");
-// Poți adăuga aici logica pentru butoane, animatii, fetch pentru evenimente etc.
